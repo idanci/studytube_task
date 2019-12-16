2a072f39afdf469005ed50bebcdc27de205d85fd
Ivan Danci <bah4uk@gmail.com>
2019-12-16 16:56:13 +0200

    Add stocks routes

---
+++ app/controllers/api/v1/stocks_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Api::V1::StockssController < Api::BaseController
+  def create
+    #
+  end
+
+  def update
+    #
+  end
+
+  def index
+    #
+  end
+
+  def destroy
+    #
+  end
+end
